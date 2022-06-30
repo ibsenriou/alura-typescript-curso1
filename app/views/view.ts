17edab1b3fb006678b39349ffcedd23fe67f492a
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-06-30 13:38:56 -0300

    Curso 2 - Conclusão da aula 3: Visibilidade de Métodos e Enumeration.

--- app/views/view.ts
+++ app/views/view.ts
@@ -6,10 +6,11 @@ export abstract class View<T> {
         this.elemento = document.querySelector(seletor);
     }
 
-    abstract template(model: T): string;
-
-    update(model: T): void {
+    public update(model: T): void {
         const template = this.template(model);
         this.elemento.innerHTML = template;
     }
+
+    protected abstract template(model: T): string;
+
 }

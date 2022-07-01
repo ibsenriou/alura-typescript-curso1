f82a7d091410cdb2c42e00f2852c7508f788c222
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-07-01 11:36:12 -0300

    Curso 3 - Conclusão da aula 2: Decorators para métodos.

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,11 +1,11 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar: boolean = false;
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
         
         if (elemento) {
@@ -14,17 +14,12 @@ export abstract class View<T> {
             throw Error('Seletor não existe no DOM. Verifique!')
         }
 
-        if (escapar) {
-            this.escapar = escapar;
-        }
     }
 
-    @logarTempoDeExecucao()
+    @logarTempoDeExecucao(true)
+    @inspect()
     public update(model: T): void {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 

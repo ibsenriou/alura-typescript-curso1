2de5df12e9862412144d1db2d11929fcd2b61a56
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-07-01 19:08:53 -0300

    Formação Typescript Concluída!

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,4 +1,6 @@
-export class Negociacao {
+import { Modelo } from "../interfaces/modelo.js";
+
+export class Negociacao implements Modelo<Negociacao>{
 
     constructor(
         private _data: Date,
@@ -7,7 +9,16 @@ export class Negociacao {
     ) {}
 
 
-    get volume():number {
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+
+        return new Negociacao(date, quantidade, valor);
+    }
+
+    get volume(): number {
         return this.quantidade * this.valor;
     }
 
@@ -16,13 +27,18 @@ export class Negociacao {
         return data;
     }
 
-    public static criaDe(dataString: string, quantidadeString: string, valorString:string): Negociacao{
-        const exp = /-/g;
-        const date = new Date(dataString.replace(exp, ','));
-        const quantidade = parseInt(quantidadeString);
-        const valor = parseFloat(valorString);
-
-        return new Negociacao(date, quantidade, valor);
+    public paraTexto(): string {
+        return `
+            Data: ${this.data},
+            Quantidade: ${this.quantidade}
+            Valor: ${this.valor}
+        `;
     }
-    
+
+    public ehIgual(negociacao: Negociacao): boolean {
+        return this.data.getDate() === negociacao.data.getDate()
+            && this.data.getMonth() === negociacao.data.getMonth()
+            && this.data.getFullYear() === negociacao.data.getFullYear();
+    }
+
 }
